ca7856a2cf966c3310819d61bbc432f035e1424f
Gavin McDonough <gavincmcd@gmail.com>
2024-02-05 21:03:39 -0500

    main.py commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from langchain.chains import LLMChain
+from langchain.llms.bedrock import Bedrock
+from langchain.prompts import PromptTemplate
+import boto3
+import os
+import streamlit as stream
+
+os.environ["AWS_PROFILE"] = "gavincmcd"
+
+modelID = "anthropopic.claud-v2"
+
+#bedrock client
+
+bedrock_client = boto3.client(
+    service_name="bedrock-runtime",
+    region_name="us-east-1"
+)
+
+llm = Bedrock(
+    model_id=modelID,
+    client=bedrock_client,
+    model_kwargs={"max_tokens_to_sample": 2000,"temperature":0.9}
+)
+
+def my_chatbot(language,freeform_text):
+    prompt = PromptTemplate(
+        input_variables=["language", "freeform_text"],
+        template="You are a chatbot. You are in {language}.\n\n{freeform_text}"
+    )
+
+    bedrock_chain = LLMChain(llm=llm, prompt=prompt)
+
+    response=bedrock_chain({'language':language, 'freeform_text':freeform_text})
+    return response
+
+#print(my_chatbot("english","who is buddha?"))
+
+stream.title("Bedrock Chatbot")
+
+language = stream.sidebar.selectbox("Language", ["english", "spanish"])
+
+if language:
+    freeform_text = stream.sidebar.text_area(label="what is your question?",
+    max_chars=100)
+
+if freeform_text:
+    response = my_chatbot(language,freeform_text)
+    stream.write(response['text'])
